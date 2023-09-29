feee304c2dcdf8587a503a1bf44d780564b8d7bf
Florencia Navarro <cecine_flor@hotmail.com>
2023-09-29 02:38:03 -0300

    adding pagination

--- src/components/ContainCards/ContainCards.jsx
+++ src/components/ContainCards/ContainCards.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import CardsMovie from "../CardMovie/CardsMovie"
+import PaginationMovieApp from "../PaginationMovieApp/PaginationMovieApp"
 
 
 function ContainCards( { nameMovie }) {
   const [ allMovies, setAllMovies ] = useState([])
+  const [ currentPage, setCurrentPage ] = useState(1)
   
   const { categoryMovie } = useParams()
 
@@ -16,13 +18,13 @@ function ContainCards( { nameMovie }) {
   
         if (categoryMovie) {
           // Si categoryMovie tiene un valor, cargar películas por categoría
-          apiUrl = `https://api.themoviedb.org/3/movie/${categoryMovie}?api_key=${import.meta.env.VITE_TMDB_APY_KEY}`
+          apiUrl = `https://api.themoviedb.org/3/movie/${categoryMovie}?api_key=${import.meta.env.VITE_TMDB_APY_KEY}&language=es-ES&page=${currentPage}`
         } else if (nameMovie) {
           // Si nameMovie tiene un valor, busca por nombre de película
-          apiUrl = `https://api.themoviedb.org/3/search/movie?query=${nameMovie}&api_key=${import.meta.env.VITE_TMDB_APY_KEY}`
+          apiUrl = `https://api.themoviedb.org/3/search/movie?query=${nameMovie}&api_key=${import.meta.env.VITE_TMDB_APY_KEY}&language=es-ES&page=${currentPage}`
         } else {
           // Si no hay categoryMovie ni nameMovie, cargar películas populares 
-          apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_APY_KEY}`
+          apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_APY_KEY}&language=es-ES&page=${currentPage}`
         }
   
         const { data } = await axios.get(apiUrl)
@@ -33,7 +35,7 @@ function ContainCards( { nameMovie }) {
     };
   
     getMovies()
-  }, [categoryMovie, nameMovie])
+  }, [categoryMovie, nameMovie, currentPage])
 
   
     return (
@@ -51,7 +53,7 @@ function ContainCards( { nameMovie }) {
           />
         ))
       )}
-        
+        <PaginationMovieApp setCurrentPage={setCurrentPage} currentPage={currentPage}/>
       </>
     )
   }
